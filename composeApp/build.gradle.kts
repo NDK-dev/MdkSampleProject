f1264c8c8ab76373b2560eb25cd44eeeef28e208
Nobuhiro Miyauchi <n.miyauchi@ndk-group.co.jp>
2024-10-04 16:51:36 +0900

    change mdk-compose version to 0.0.0-alph03

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,7 +66,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation("jp.co.ndk_group.lib:mdk-compose:0.0.0")
+            implementation("jp.co.ndk_group.lib:mdk-compose:0.0.0-alpha03")
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
