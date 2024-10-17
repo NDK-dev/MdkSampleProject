43b89e8e83c47dadd258822450f43ad630a75630
Kazuhiro ISHIBASHI <k.ishibashi@ndk-group.co.jp>
2024-10-17 11:14:15 +0900

    Remove unused classes

--- composeApp/src/desktopMain/kotlin/jp/co/ndk_group/mdk/sample/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package jp.co.ndk_group.mdk.sample
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
