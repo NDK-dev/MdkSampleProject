d5cee72b20670ec277b1a2196ee7fa5f2647f3fb
Nobuhiro Miyauchi <n.miyauchi@ndk-group.co.jp>
2025-03-04 10:49:02 +0900

    Fix mdk dependency to use mdk.compose in build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,7 +66,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation(libs.mdk)
+            implementation(libs.mdk.compose)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
