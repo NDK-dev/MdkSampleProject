fab9b6f8926394c8a80136a5d1b01f8713f747d2
Nobuhiro Miyauchi <n.miyauchi@ndk-group.co.jp>
2025-03-11 12:12:16 +0900

    Remove unused imports from MainActivity.kt
    - Clean up the code by removing unnecessary import statements.
    - Improve readability and maintainability of MainActivity.kt.

--- sample/compose/app/src/main/java/jp/co/ndk_group/mdk/sample/MainActivity.kt
+++ sample/compose/app/src/main/java/jp/co/ndk_group/mdk/sample/MainActivity.kt
@@ -4,26 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.unit.dp
-import jp.co.ndk_group.mdk.MdkOptions
-import jp.co.ndk_group.mdk.MdkResult
-import jp.co.ndk_group.mdk.MdkTarget
-import jp.co.ndk_group.mdk.MdkView
-import jp.co.ndk_group.mdk.entity.MdkSide
 import jp.co.ndk_group.mdk.sample.ui.theme.MdkSampleProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,4 +18,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
